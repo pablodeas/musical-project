7cb5d43342954617aae6cbbd47a8563f9540d66a
Pablo de Andrade Silva <pablodeas@gmail.com>
2023-12-29 17:08:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// Screen Sound Program
+
+// List
+//List<string> listBands = new List<string>  {"U2", "Mastodon", "Gojira", "Evanescence"};
+
+Dictionary<string, List<int>> registeredBands = new Dictionary<string, List<int>>();
+
+//Function to Create a Title Modified
+void BeautifyTitle(string title)
+{
+    int numberOfLetters = title.Length;
+    string asteriscos = string.Empty.PadLeft(numberOfLetters, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(title);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+// Logo
+void WelcomeMessage()
+{
+    BeautifyTitle("☕️ Screen Sound.📟");
+}
+
+// Function of the Menu with a Loop inside
+void MenuMessage()
+{
+    WelcomeMessage();
+
+    Console.WriteLine("- 1 to -> Register a New Band.");
+    Console.WriteLine("- 2 to -> Show All Bands");
+    Console.WriteLine("- 3 to -> Avaluate a Band");
+    Console.WriteLine("- 4 to -> Show Average Score of a Band");
+    Console.WriteLine("- 0 to -> Exit\n");
+
+    Console.Write("-> ");
+    string opt = Console.ReadLine()!;
+    
+    // String converted to numeric to use in SWITCH
+    int numbOpt = int.Parse(opt);
+
+    switch(numbOpt)
+    {
+        case 1: RegisterBand();
+            break;
+        case 2: ShowBands();
+            break;
+        case 3: Console.WriteLine("\nYou choose option number: " + numbOpt + "\n");
+            break;
+        case 4: Console.WriteLine("\nYou choose option number: " + numbOpt + "\n");
+            break;
+        case 0: Console.WriteLine("\nClosing the program\nGood-Bye!\n");
+            break;
+        default: Console.WriteLine("\nWrong option!\n");
+            break;
+    }
+}
+
+// Function to Register a New Band in the List
+void RegisterBand()
+{
+    Console.Clear();
+
+    BeautifyTitle("Register of Bands.");
+
+    Console.Write("\nWrite the name of the Band: ");
+    string newBand = Console.ReadLine()!;
+    registeredBands.Add(newBand, new List<int>());
+    Console.Write($"The Band {newBand} were Registered.");
+
+    Thread.Sleep(2000);
+    Console.Clear();
+    MenuMessage();
+
+}
+
+
+// Function to Show all Bands Registered
+void ShowBands()
+{
+    Console.Clear();
+
+    BeautifyTitle("All Bands.");
+
+    foreach (string band in registeredBands.Keys)
+    {
+        Console.WriteLine($"-> {band}");
+    }
+
+    Console.WriteLine("\nWrite something to return.");
+    Console.ReadKey();
+    Console.Clear();
+
+    MenuMessage();
+}
+
+
+// Call the Menu
+MenuMessage();
---
+++ README.md
@@ -0,0 +1,60 @@
+# Screen Sound Program
+
+## Overview
+
+Welcome to the Screen Sound Program! This simple console application allows you to register and evaluate bands, showing a list of registered bands and their average scores.
+
+## Features
+
+- **Register a New Band**: Add a new band to the program.
+- **Show All Bands**: Display a list of all registered bands.
+- **Avaluate a Band**: Evaluate and provide feedback for a specific band.
+- **Show Average Score of a Band**: View the average score of a particular band.
+- **Exit**: Close the program.
+
+## Usage
+
+1. Clone the repository or download the script.
+2. Run the script in a C# environment.
+
+## How to Use
+
+Upon running the script, you will be presented with a menu with various options. Use the corresponding numbers to navigate through the features.
+
+- To register a new band, select option `1` and follow the instructions.
+- To view all registered bands, select option `2`.
+- For band evaluation and average scores, choose options `3` and `4` respectively.
+- To exit the program, select option `0`.
+
+## Example
+
+```csharp
+Welcome to ☕️ Screen Sound.📟
+
+- 1 to -> Register a New Band.
+- 2 to -> Show All Bands
+- 3 to -> Avaluate a Band
+- 4 to -> Show Average Score of a Band
+- 0 to -> Exit
+
+-> 1
+
+Register of Bands.
+Write the name of the Band: [Enter band name]
+The Band [band name] was Registered.
+
+[Menu returns after a short delay]
+
+...
+
+- 2 to -> Show All Bands
+- 3 to -> Avaluate a Band
+- 4 to -> Show Average Score of a Band
+- 0 to -> Exit
+
+```
+
+## Notes
+
+- The program uses a simple console interface.
+- Band names are registered in a dictionary, allowing for easy retrieval and modification.
